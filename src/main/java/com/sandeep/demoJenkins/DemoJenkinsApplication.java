671b0fbf02c4a182b7b775a6d280c4a27270becd
Sandeep Mahto <san14deep88@gmail.com>
2025-11-23 21:36:59 +0530

    commiting application

---
+++ src/main/java/com/sandeep/demoJenkins/DemoJenkinsApplication.java
@@ -0,0 +1,15 @@
+package com.sandeep.demoJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsApplication.class, args);
+
+        System.out.println("Hello World");
+	}
+
+}
